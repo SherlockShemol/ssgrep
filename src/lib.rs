1fc3e6278e4b425f00937c3ff72d7a9466ee8e9b
Sa Song <shemol106@gmail.com>
2024-12-31 11:50:41 +0800

    update full func

--- src/lib.rs
+++ src/lib.rs
@@ -1,78 +1,78 @@
+use clap::{ArgAction, Parser};
+use std::env;
 use std::error::Error;
 use std::fs;
-use std::env;
-pub struct Config<'a>{
+
+#[derive(Parser)]
+#[command(
+    name = "SSgrep Program",
+    version = "0.0.1",
+    author = "Sherlock Shemol <shemol106@gmail.com>",
+    about = "Search for matched context in a file.For author to learn Rust."
+)]
+pub struct Cli {
+    pub query: String,
+
+    #[arg(short = 'f', long = "file", action = ArgAction::Set, help = "Specify the file to search.")]
+    pub file: String,
+
+    #[arg(short = 'i', long = "ignore_case", action = ArgAction::SetTrue, help = "Ignore case when searching.")]
+    pub ignore_case: bool,
+}
+
+pub struct Config<'a> {
     pub query: &'a String,
     pub file_path: &'a String,
-    pub ignore_case:bool,
+    pub ignore_case: bool,
 }
 
-impl<'a> Config<'a>{
-    pub fn build(args:&'a[String]) -> Result<Config<'a>,&str>{
-        if args.len() <3{
-            return Err("not enough arguments");
-        }
-        let query = &args[1];
-        let file_path = &args[2];
-        let ignore_case = env::var("IGNORE_CASE").is_ok();  
-        
-        Ok(Config{query,file_path,ignore_case})
+impl<'a> Config<'a> {
+    pub fn build(cli: &'a Cli) -> Result<Config<'a>, &str> {
+        let query = &cli.query;
+        let file_path = &cli.file;
+        let ignore_case = cli.ignore_case || env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
-pub fn run(config:Config)->Result<(),Box<dyn Error>>{
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have able to read the file.");
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have able to read the file.");
 
-    let results = if config.ignore_case{
+    let results = if config.ignore_case {
         search_insensitive(&config.query, &contents)
-    }else{
+    } else {
         search(&config.query, &contents)
     };
-    
-    for line in results{
+
+    for line in results {
         println!("{line}");
     }
-    
+
     Ok(())
 }
 
-pub fn search<'a>(query:&str,contents:&'a str) -> Vec<&'a str>{
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
-    for line in contents.lines(){
-        if line.contains(&query){
-           results.push(line); 
+    for line in contents.lines() {
+        if line.contains(&query) {
+            results.push(line);
         }
     }
     results
 }
 
-pub fn search_insensitive<'a>(query:&str,contents:&'a str) -> Vec<&'a str>{
+pub fn search_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
-    for line in contents.lines(){
-        if line.to_lowercase().contains(&query.to_lowercase()){
-           results.push(line); 
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()) {
+            results.push(line);
         }
     }
     results
-}
-
-mod tests{
-    use super::*;
-
-    #[test]
-    fn one_result(){
-        let query = "duct";
-        let contents = "Rust:\nsafe, fast, productive.\nPick three.";
-
-        assert_eq!(vec!["safe, fast, productive."],search(query,contents));
-    }
-    
-    #[test]
-    fn case_insensitive(){
-        let query = "rUsT";
-        let contents = "Rust:\nsafe, fast, productive.\nPick three.\nTrust me.";
-
-        assert_eq!(vec!["Rust:","Trust me."],search_insensitive(query, contents));
-    }
 }
